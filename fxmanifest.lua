77f3984bb952853fe6ccbe51eab11cdc6ffd90eb
SraxDev <102546354+SraxDev@users.noreply.github.com>
2023-07-04 23:58:55 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SraxDev'
+description 'FiveM resource that reduces the density of NPCs and traffic in your server.'
+version 'v1.0'
+
+client_script 'reduce_density.lua'
---
+++ reduce_density.lua
@@ -0,0 +1,22 @@
+local Config = {
+    pedDensity = 0,
+    scenarioPedDensity = 0,
+    vehicleDensity = 0,
+    randomVehicleDensity = 0,
+    parkedVehicleDensity = 0
+}
+
+Citizen.CreateThread(function()
+    while true do
+        -- Réglage de la densité des PNJ
+        SetPedDensityMultiplierThisFrame(Config.pedDensity)
+        SetScenarioPedDensityMultiplierThisFrame(Config.scenarioPedDensity, Config.scenarioPedDensity)
+
+        -- Réglage de la densité de la circulation
+        SetVehicleDensityMultiplierThisFrame(Config.vehicleDensity)
+        SetRandomVehicleDensityMultiplierThisFrame(Config.randomVehicleDensity)
+        SetParkedVehicleDensityMultiplierThisFrame(Config.parkedVehicleDensity)
+        
+        Wait(0)
+    end
+end)
