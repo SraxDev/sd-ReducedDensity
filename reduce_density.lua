945b50fa79caace99bf11f1873dd27e2af06efdd
SraxDev <102546354+SraxDev@users.noreply.github.com>
2025-05-22 00:46:35 +0200

    Update reduce_density.lua

--- reduce_density.lua
+++ reduce_density.lua
@@ -1,21 +1,30 @@
 local Config = {
     pedDensity = 0,
     scenarioPedDensity = 0,
-    vehicleDensity = 0,              -- Max value 1.0
+    vehicleDensity = 0,            --- Max value 1.0
     randomVehicleDensity = 0,
     parkedVehicleDensity = 0
 }
 
+local setters = {
+    {key = 'pedDensity', func = SetPedDensityMultiplierThisFrame},
+    {key = 'scenarioPedDensity', func = function(v) SetScenarioPedDensityMultiplierThisFrame(v, v) end},
+    {key = 'vehicleDensity', func = SetVehicleDensityMultiplierThisFrame},
+    {key = 'randomVehicleDensity', func = SetRandomVehicleDensityMultiplierThisFrame},
+    {key = 'parkedVehicleDensity', func = SetParkedVehicleDensityMultiplierThisFrame}
+}
+
+local current = {}
+
 Citizen.CreateThread(function()
     while true do
-        -- Mise à jour moins fréquente (500ms au lieu de chaque frame)
-        Wait(500)
+        Wait(1000)
         
-        -- Réglage de la densité
-        SetPedDensityMultiplierThisFrame(Config.pedDensity)
-        SetScenarioPedDensityMultiplierThisFrame(Config.scenarioPedDensity, Config.scenarioPedDensity)
-        SetVehicleDensityMultiplierThisFrame(Config.vehicleDensity)
-        SetRandomVehicleDensityMultiplierThisFrame(Config.randomVehicleDensity)
-        SetParkedVehicleDensityMultiplierThisFrame(Config.parkedVehicleDensity)
+        for _, setting in ipairs(setters) do
+            if current[setting.key] ~= Config[setting.key] then
+                setting.func(Config[setting.key])
+                current[setting.key] = Config[setting.key]
+            end
+        end
     end
 end)
